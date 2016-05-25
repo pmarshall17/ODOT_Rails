f2b307e2ae95058bf1c4059153b512f91e12d06a
Pat Marshall <pmarshall1716@gmail.com>
2016-05-25 00:57:07 -0600

    Final product

---
+++ db/migrate/20160525062218__add_completed_at_to_todo_items.rb
@@ -0,0 +1,4 @@
+class AddCompletedAtToTodoItems < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525050413) do
+ActiveRecord::Schema.define(version: 20160525062218) do
 
   create_table "todo_items", force: :cascade do |t|
     t.integer  "todo_list_id"
